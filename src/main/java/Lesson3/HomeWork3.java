1f7e0d1c768c7feb3767dd5543e0b2a38e0b3800
Alex <alecfyz@gmail.com>
2021-02-18 14:06:26 +0300

    HomeWork for Lesson 3. Guess the fruit. Reviewed_2.

--- src/main/java/Lesson3/HomeWork3.java
+++ src/main/java/Lesson3/HomeWork3.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class HomeWork3 {
 
     static Scanner scanner = new Scanner(System.in);
+    static String masked = "###############";
+    static boolean matched = false;
 
     public static void main(String[] args) {
         String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
@@ -12,20 +14,17 @@ public class HomeWork3 {
         System.out.println("word = '" + word + "'");
         System.out.println("О, повелитель, отгадай фрукт, загаданный Шахразадой!");
         String userword;
-        boolean matched = false;
+
         while(!matched) {
             userword = getUserWord();
-
-            if (userword.equals(word)) {  // FALSE!!!
+            if (userword.equals(word)) {
                 System.out.printf("Да! Ты отгадал, о свет моих очей! Это слово '%s'!%n Сказка и вопросы на сегодня закончены, повелитель!", word);
                 matched = true;
                 break;
             } else {
-                System.out.println("Нет, господин, продолжаем");
                 showCompareWords(word, userword);
             }
         }
-
     }
 
     private static int GetRandomEl(String[] words) {
@@ -37,17 +36,30 @@ public class HomeWork3 {
     }
 
     private static void showCompareWords(String word, String userword) {
-        String masked = "";
+        String workmask = "";
         int maxLen = Math.min(word.length(), userword.length());
         int guessed = 0;
         for (int i = 0; i < maxLen; i++) {
-            if (word.charAt(i) == userword.charAt(i)) {
-                masked += word.charAt(i);
+
+            if (masked.charAt(i) == word.charAt(i))
+            {
+                workmask += masked.charAt(i);
+//                System.out.println("!!!! " + word.charAt(i)  + " !!!!!!");
                 guessed++;
-            } else masked +="#";
+            } else if (word.charAt(i) == userword.charAt(i)) {
+
+                workmask += word.charAt(i);
+                guessed++;
+            } else workmask +="#";
+        }
+        masked = workmask;
+        if (masked.equals(word)){
+            matched = true;
+            System.out.printf("Да! Ты усерден, о дыхание моего ветра, и по буквам составил это слово: '%s'!%n Сказка и вопросы на сегодня закончены, повелитель!", word);
+            return;
         }
         for (int j = maxLen; j < 15; j++) { masked += "#"; }
-        System.out.printf("Всего вы угадали %d букв из этого слова %n и вот эти буквы: %s%nПопробуй еще раз, светлейший!%n", guessed, masked);
+        System.out.printf("Всего вы угадали %d букв(ы) из этого слова %n и вот эти буквы: %s%nПопробуй еще раз, светлейший!%n", guessed, masked);
     }
 
 }
