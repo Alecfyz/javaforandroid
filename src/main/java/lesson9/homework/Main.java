37ee40defc498d6bd9ab64cd9c8fb161ffa67c69
Alex <alecfyz@gmail.com>
2021-03-23 13:38:28 +0300

    HomeWork for Lesson 9. REv5

--- src/main/java/lesson9/homework/Main.java
+++ src/main/java/lesson9/homework/Main.java
@@ -6,32 +6,45 @@ import java.util.Random;
 
 public class Main {
     private static String[][] myArr;
-    private static String[] srcArr = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "$", "&", "@", "*", "dsa", " "}; // source array
+    private static String[] RightSrcArr = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"}; // source array
+    private static String[] WrongSrcArr = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "#"}; // source array
     static final Random random = new Random();
-    static final int SIZE = 6; // array side
+    static final int SIZE = 4; // array side
+    static final boolean TEST = false;
 
     public static void main(String[] args) {
-        int summ;
-        myArr = generateSquareArray(false);
+        int summ = 0;
+        myArr = generateSquareArray(TEST ? WrongSrcArr : RightSrcArr);
+        try {
+            summ = workWithArray(myArr);
+            System.out.printf("Результат: сумма всех численных элементов массива = %d%n", summ);
 
-        summ = workWithArray(myArr);
-        System.out.printf("Результат: сумма всех численных элементов массива = %d%n", summ);
+        } catch (MyArraySizeException e) {
+            System.out.println(e.toString());
+        } catch (MyArrayDataException e) {
+            System.out.println(e.toString());
+        }
+
     }
 
-    private static int workWithArray(String[][] ar) {
+    private static int workWithArray(String[][] ar) throws MyArraySizeException, MyArrayDataException {
+        if (ar.length > 4)
+            throw new MyArraySizeException(ar.length);
+
         int holeSumm = 0;
-        int curInt;
+        int curInt = 0;
 
         for (int i = 0; i < ar.length; i++) {
             for (int j = 0; j < ar.length; j++) {
-                curInt = 0;
+                if (j > 3)
+                    throw new MyArraySizeException(i);
+
                 try {
-                    curInt = getNextInt(ar[i][j], i, j);
-                } catch (MyArraySizeException e2) {
-                    System.err.println(e2);
-                    break; // элементы сверх лимита (4) не считаем
-                } catch (MyArrayDataException e1) {
-                    System.err.println(e1);
+                    curInt = Integer.parseInt(String.valueOf(ar[i][j]));
+                } catch (NumberFormatException e1) {
+                    curInt = 0;
+                    throw new MyArrayDataException("строка '" + ar[i][j] + "' в позиции " + i + "," + j);
+
                 }
                 holeSumm += curInt;
             }
@@ -39,41 +52,12 @@ public class Main {
         return holeSumm;
     }
 
-    private static int getNextInt(String str, int row, int col) throws MyArraySizeException, MyArrayDataException {
-        if (col > 4)
-            throw new MyArraySizeException(row);
-
-        int curint;
-        try {
-            curint = Integer.parseInt(String.valueOf(str));
-        } catch (NumberFormatException e1) {
-            curint = 0;
-            throw new MyArrayDataException("строка '" + str + "' в позиции " + row + "," + col);
-        }
-        return curint;
-    }
-
-    private static String[][] generateSquareArray(boolean test) {
-        if (test) {
-            String[][] myArr = {
-                    {"3", "3", "3", "@", "4", "0"},
-                    {"3", "$", "&", "4", "dsa", "2"},
-                    {"$", "0", "3", "5", " ", "0"},
-                    {"8", "0", "4", "2", "&", " "},
-                    {"1", "$", "2", "5", "8", "9"},
-                    {"@", "9", "5", "9", "1", "3"}
-            };
-            Arrays.stream(myArr).map(Arrays::toString).forEach(System.out::println);
-            return myArr;
-        }
-
+    private static String[][] generateSquareArray(String[] srcArr) {
         myArr = new String[SIZE][SIZE];
         for (int i = 0; i < SIZE; i++) {
             for (int j = 0; j < SIZE; j++) {
                 myArr[i][j] = srcArr[random.nextInt(srcArr.length)];
-//                System.out.printf("\"%s\",", myArr[i][j]);
             }
-//            System.out.printf("%n");
         }
         Arrays.stream(myArr).map(Arrays::toString).forEach(System.out::println);
 
